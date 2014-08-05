514083a8545585e42e92c3550d5f48eab91740e3
ungdailong <ungdailong@gmail.com>
2014-08-05 15:13:58 +0700

    commit index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 /*
  *---------------------------------------------------------------
- * APPLICATION ENVIRONMENT
+ * APPLICATION ENVIRONMENT1
  *---------------------------------------------------------------
  *
  * You can load different configurations depending on your
